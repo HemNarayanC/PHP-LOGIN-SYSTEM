1966be26e32eccb1125a235ddc6961e24cf5fd51
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2024-09-07 14:07:02 +0545

    db connetion and navbar

---
+++ partials/_nav.php
@@ -0,0 +1,48 @@
+<?php
+
+// start_session();
+
+if(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']=true){
+  $loggedIn = true;
+}
+
+else{
+  $loggedIn = false;
+}
+
+  echo '<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+        </li>';
+       if(!$loggedIn){
+        echo '<li class="nav-item">
+          <a class="nav-link" href="login.php">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="signup.php">Sign Up</a>
+        </li>';
+       }
+       
+        if($loggedIn){
+          echo '<li class="nav-item">
+            <a class="nav-link" href="logout.php">Logout</a>
+          </li>';
+        }
+
+      echo '</ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>';
+
+?>
